0afd2483b9d226fa61e58339160ee1fd09217882
AdamClarke0 <AdamClarke0@users.noreply.github.com>
2016-05-13 20:40:06 +0100

    Create addstyles.js

---
+++ addstyles.js
@@ -0,0 +1,4 @@
+var css = document.createElement("style")
+css.type = "text/css";
+css.href = "https://raw.githubusercontent.com/AdamClarke0/files/master/styles.css"
+document.body.appendChild(css);
