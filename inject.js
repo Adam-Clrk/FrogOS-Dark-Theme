9d54a27f20fc598b96bd481d0603b4818d314e8d
Adam Clarke <9417231+Adam-Clrk@users.noreply.github.com>
2017-10-18 23:31:08 +0100

    update urls

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
 var css = document.createElement("style")
 css.type = "text/css";
-css.href = "https://raw.githubusercontent.com/AdamClarke0/files/master/styles.css"
+css.href = "https://raw.githubusercontent.com/Adam-Clrk/files/master/styles.css"
 document.body.appendChild(css);
